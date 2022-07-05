c513736fabfd082d13c9543200bb4e1bfd0730ac
Yura Aaltink <yuraaltink@gmail.com>
2022-07-05 13:01:51 +0100

    Added test to makefile

--- vb/vb/app.py
+++ vb/vb/app.py
@@ -1,6 +1,15 @@
 from aiohttp import web
 
 
+
+class MyView(web.View):
+    async def get(self):
+        return await get_resp(self.request)
+
+    async def post(self):
+        return await post_resp(self.request)
+
+
 async def handle(request):
     name = request.match_info.get("name", "Anonymous")
     text = "Hello, " + name
@@ -9,5 +18,6 @@ async def handle(request):
 
 def create_app():
     app = web.Application()
+    app.add_routes([web.view("/test", MyView)])
     app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
     return app
