c1745b2c3f93981163cd15852ddc12f9aa4ec03b
Yura Aaltink <yuraaltink@gmail.com>
2022-07-25 07:09:52 +0100

    Starting failing unit test

--- vb/vb/app.py
+++ vb/vb/app.py
@@ -13,8 +13,8 @@ class VersionView(web.View):
         try:
             version = VersionModel(**json_data)
         except ValidationError as ex:
-            return web.json_response(ex.dict())
-            pass
+            return web.json_response(str(ex))
+            
 
         Version.create(**version.dict()).save()
         return web.json_response(version.dict(), content_type='application/json')
